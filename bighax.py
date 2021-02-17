454c2005a79cd0e51ae18214b700fc87c62c46b5
OnlyOne101 <78464899+OnlyOne101@users.noreply.github.com>
2021-02-18 00:05:37 +1300

    Add files via upload

---
+++ bighax.py
@@ -0,0 +1,14 @@
+import os
+
+you = input("1337 or not. Type one: ")
+os.system("cls")
+
+if you != "1337":
+	print("Noob lol")
+	print("---------\n\n")
+	you = "Noob"
+	while you == "Noob":
+		print("Hacking your Toaster...")
+		break
+else:
+	print("Good Boy")
